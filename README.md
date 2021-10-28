c4f1dbe9773841a668ad063ae154887cd042ad98
erhannis <eyeillus@gmail.com>
2021-10-27 23:34:50 -0400

    ...Spring sucks; I give up.  Off to find a better framework.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # spring-boot-user-account-registration
 User Account Registration with Spring Boot
 
+
+sudo mysql --password
+
 CREATE USER 'dbUser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dbPass';
 create database demo_db;
 grant all on demo_db.* to 'dbUser'@'localhost';
+
--- user-account-registration/src/main/java/com/codebyamir/configuration/SecurityConfiguration.java
+++ user-account-registration/src/main/java/com/codebyamir/configuration/SecurityConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +25,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
   	private UserService userService;
+    private SessionRepository sessionRepository;
 
 	@Autowired
-	public SecurityConfiguration(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService) {
+	public SecurityConfiguration(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService, SessionRepository sessionRepository) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 		this.userService = userService;
+        this.sessionRepository = sessionRepository;
 	}
     
 	@Override
@@ -36,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         		.antMatchers("/register").permitAll()
     			.antMatchers("/confirm").permitAll()
 				.antMatchers("/").permitAll()
-				.anyRequest().authenticated()
+				.anyRequest().permitAll()//.authenticated()
 				.and()
 			.formLogin()
 				.loginPage("/login")
@@ -53,21 +57,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         builder.authenticationProvider(new AuthenticationProvider() {
             @Override
             public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                Authentication result = null;
+                //Authentication result = null;
                 String principal = (String)authentication.getPrincipal();
                 String credentials = (String)authentication.getCredentials();
-                ((WebAuthenticationDetails)authentication.getDetails()).getSessionId();
+                String sessionId = ((WebAuthenticationDetails)authentication.getDetails()).getSessionId();
+                Session session = sessionRepository.findById(sessionId);
+                if (session != null) {
+                    return authentication;
+                }
         		User userExists = userService.findByEmail(principal);
                 if (userExists == null) {
                     throw new BadCredentialsException("Invalid credentials");
                 }
+                if (credentials == null) {
+                    throw new BadCredentialsException("No credentials?");
+                }
                 if (bCryptPasswordEncoder.matches(credentials, userExists.getPassword())) {
                     //authentication.setAuthenticated(true);
-                    result = authentication;
+                    session = sessionRepository.createSession();
+                    
+                    return authentication;
                 } else {
                     throw new BadCredentialsException("Invalid credentials");
                 }
-                return result;
             }
 
             @Override
